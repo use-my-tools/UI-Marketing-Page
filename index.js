808aafa7bf85500525fb939ba529162cd7ffb1cf
Patrick <patrick.schwindt14@gmail.com>
2019-02-04 18:38:28 -0700

    A couple animations

--- index.js
+++ index.js
@@ -2,4 +2,11 @@
 
 //Variables
 
-const button = document.querySelector('.button');
+const headerButton = document.querySelector('.main-btn');
+const bgImage = document.querySelector('.bg-wrapper');
+
+
+TweenLite.from ('.logo', 3, {y:-200});
+TweenLite.from ('.intro-banner-info', 3, {y:200});
+TweenMax.set(bgImage, {opacity: 0});
+TweenMax.to(bgImage, 4, {opacity: 1});
