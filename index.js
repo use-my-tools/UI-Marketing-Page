a1fd98df10c0d6dcd3f2572d609ebdab26927f32
Patrick <patrick.schwindt14@gmail.com>
2019-02-05 13:16:02 -0700

    Latest

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
 
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.use(express.static(path.join(__dirname)));
-//app.use("/styles", express.static(__dirname));
-//app.use("/images", express.static(__dirname + '/Images'));
-//app.use("/scripts", express.static(__dirname + '/scripts'));
-
-// viewed at based directory http://localhost:8080/
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-// add other routes below
-// app.get('/about', function (req, res) {
-//   res.sendFile(path.join(__dirname + 'views/about.html'));
-// });
-
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
 
 
 //Variables//
