d131a2ded8e70a076236bc3c8d43e9af98c7ac10
Patrick <patrick.schwindt14@gmail.com>
2019-02-05 12:43:35 -0700

    edit

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ app.get('/', function (req, res) {
 //   res.sendFile(path.join(__dirname + 'views/about.html'));
 // });
 
-server.listen(process.env.PORT || port);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 
 //Variables//
